c2ee78a3824f4ecb5f84b05178966175a4aea4d4
annaola <anna.biadasiewicz@gmail.com>
2017-11-11 23:54:07 +0100

    Some fixes

--- izp/polls/views.py
+++ izp/polls/views.py
@@ -41,7 +41,37 @@ def result(request, question_id):
     return render(request, 'polls/result.html', {'question': question, 'choices': choices, 'codes': codes})
 
 
-def voting(question, choice, code):
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    if question.start_date > timezone.now() or question.end_date < timezone.now():
+        return render(request, 'polls/detail.html',
+                {'question': question, 'error': "Głosowanie nie jest aktywne"})
+
+    code = request.POST['code']
+    if code == '' or not question.is_code_correct(code):
+        return render(request, 'polls/detail.html',
+                {'question': question, 'error': "Niewłaściwy kod uwierzytelniający"})
+
+    choice = request.POST.get('choice', None)
+    new_choice = request.POST['new_choice']
+    if(choice and new_choice != ''):
+        return render(request, 'polls/detail.html',
+                {'question': question, 'error': "Nie można głosować na istniejącą odpowiedź i jednocześnie proponować nową"})
+
+    if not choice and new_choice == '':
+        return render(request, 'polls/detail.html',
+                {'question': question, 'error': "Nie wybrano odpowiedzi"})
+
+    if choice:
+        if question.choice_set.filter(pk=choice).exists():
+            choice = question.choice_set.get(pk=choice)
+        else:
+            return render(request, 'polls/detail.html',
+                    {'question': question, 'error': "Odpowiedź nie istnieje"})
+
+    if not choice and OpenQuestion.objects.filter(pk=question.pk).exists():
+        choice = Choice.objects.create(question=question, choice_text=new_choice)
+
     prev_vote = Vote.objects.filter(question__exact=question, code__exact=code).last()
     if prev_vote:
         prev_vote.choice.votes -= 1
@@ -51,58 +81,4 @@ def voting(question, choice, code):
     choice.votes += 1
     choice.save()
     Vote.objects.create(question=question, choice=choice, code=code)
-
-
-def vote(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    if question.start_date > timezone.now() or question.end_date < timezone.now():
-        return render(request, 'polls/detail.html', {
-            'question': question, 'error': "Głosowanie nie jest aktywne"})
-    try:
-        openQuestion = OpenQuestion.objects.get(pk=question_id)
-    
-    except OpenQuestion.DoesNotExist:   # case for "basic" question
-        try:
-            choice = question.choice_set.get(pk=request.POST['choice'])
-            code = request.POST['code']
-            if code == '' or not question.is_code_correct(code):
-                raise AttributeError
-
-        except (KeyError, Choice.DoesNotExist):
-            return render(request, 'polls/detail.html', {'question': question, 'error': "Nie wybrano odpowiedzi"})
-
-        except AttributeError:
-            return render(request, 'polls/detail.html',
-                          {'question': question, 'error': "Niewłaściwy kod uwierzytelniający"})
-
-        else:
-            voting(question, choice, code)
-            return HttpResponseRedirect(reverse('polls:index'))
-    else:   #case for OpenQuestion
-        try:
-            code = request.POST['code']
-            if code == '' or not question.is_code_correct(code):
-                raise AttributeError
-            new_choice = request.POST['new_choice']
-            choice = question.choice_set.get(pk=request.POST['choice'])
-        except AttributeError:
-            return render(request, 'polls/detail.html',
-                  {'question': question, 'error': "Niewłaściwy kod uwierzytelniający", 'is_open': True})
-        except (KeyError, Choice.DoesNotExist): # case for an unselected radio button
-            if new_choice == '':    # no answer
-                raise KeyError
-            else:                   # new choice selected
-                new_choice = question.choice_set.create(choice_text=new_choice)
-                voting(question, new_choice, code)
-                return HttpResponseRedirect(reverse('polls:index'))
-
-        except (KeyError, Choice.DoesNotExist): # no answer selected
-            return render(request, 'polls/detail.html', {'question': question, 'error': "Nie wybrano odpowiedzi", 'is_open': True})
-        else:
-            if new_choice == '':
-                voting(question, choice, code)
-                return HttpResponseRedirect(reverse('polls:index'))
-            else:
-                return render(request, 'polls/detail.html',
-                              {'question': question, 'error': "Nie można udzielić dwóch głosów", 'is_open': True})
-    
+    return HttpResponseRedirect(reverse('polls:index'))
