884c680e2ddf0a2ffb92d4653aa670f772495d78
valentinebaliuk <valentyn.baliuk@yahoo.com>
2017-11-26 16:47:56 +0200

    Form validation when create a Question
    data validation  
    No possibility to have 2 open votings in one time.

---
+++ izp/polls/forms.py
@@ -0,0 +1,22 @@
+from django import forms
+from .models import Question
+
+
+class QuestionAdminForm(forms.ModelForm):
+    class Meta:
+        model = Question
+        fields = '__all__'
+    def clean(self):
+        question_text = self.cleaned_data.get('question_text')
+        start_date = self.cleaned_data.get('start_date')
+        end_date = self.cleaned_data.get('end_date')
+        time = self.cleaned_data.get('time')
+        max_end_date = Question.objects.latest('end_date').end_date
+        if start_date and max_end_date > start_date:
+            raise forms.ValidationError('Nie wolno tworzyć ' + 
+                                        'czasowo nakładających ' + 
+                                        'się głowowań. ' + 
+                                        'Najbliższa możliwa ' +  
+                                        'data rozpoczencia ' +
+                                        'nowego głosowania ' +
+                                         str(max_end_date))
