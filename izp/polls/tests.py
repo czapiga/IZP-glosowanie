6eeba54c3f4ce631449dd123dd781274e8ffeeb6
Fanderman <33103954+Fanderman@users.noreply.github.com>
2017-11-28 00:54:19 +0100

    Update tests.py

--- izp/polls/tests.py
+++ izp/polls/tests.py
@@ -299,26 +299,26 @@ class OpenQuestionVoteViewTests(TestCase):
         response = self.client.post(
             url, {'code': password,
                   'new_choice': 'odpowiedz'})
-        self.assertIs(Choice.objects.all().count(), 1)
-        self.assertIs(Choice.objects.all()[0].votes, 2)
+        self.assertIs(Choice.objects.all().count(), 3)
+        self.assertIs(Choice.objects.all()[2].votes, 2)
 
     def test_vote_two_similar_answers(self):
         """
         If two similar but not the same open answers are written in two votes,
         it counts as two different answers with one vote each.
         """
-        new_question = OpenQuestion.objects.create(question_text="OpenQuest")
+        new_question = OpenQuestion.objects.get(question_text="OpenQuest")
         new_question.save()
         url = reverse('polls:vote', args=(new_question.id,))
-        password = new_question.accesscode_set.all()[0].code
+        password = new_question.get_codes()[0]
         response = self.client.post(
             url, {'code': password,
                   'new_choice': 'odpowiedz2'})
-        password = new_question.accesscode_set.all()[1].code
+        password = new_question.get_codes()[1]
         response = self.client.post(
             url, {'code': password,
                   'new_choice': 'odpowiedz'})
-        self.assertIs(Choice.objects.all().count(), 2)
+        self.assertIs(Choice.objects.all().count(), 4)
         for c in Choice.objects.all():
             self.assertIs(c.votes, 1)
 
